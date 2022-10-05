6c37ceed021b2e761eaaf898225aeda5c61580e8
chauler <amt1309@gmail.com>
2022-10-05 12:25:27 -0500

    Completed working A*

--- pathfind.js
+++ pathfind.js
@@ -2,82 +2,136 @@ document.querySelector("#generate").addEventListener("click", () => {
     FindPath(document.querySelector("tbody"))
 })
 
-class Node {
-    constructor(x, y, walkable) {
+class Vertex {
+    constructor(x, y, walkable, start=false, end=false) {
         this.x = x
         this.y = y
-        this.f = Infinity
-        this.g = Infinity
+        this.f = 999999999
+        this.g = 999999999
         this.h = 0
         this.walkable = walkable
+        this.start = start
+        this.end = end
+    }
+}
+
+class Graph {
+    constructor() {
+        this.adjList = new Map()
+        this.vertices = []
+    }
+
+    addVertex(vertex) {
+        this.vertices.push(vertex)
+        this.adjList.set(vertex, [])
+    }
+
+    addEdge(a, b) {
+        this.adjList.get(a).push(b)
+        this.adjList.get(b).push(a)
     }
 }
 
 function FindPath(table)
 {
-    const nodes = []
+    let start = undefined
+    let end = undefined
+
+    const graph = new Graph()
     table.childNodes.forEach(row => {
-        let temp = []
         row.childNodes.forEach((cell) => {
-            if(cell.className !== "wall") {
-                coords = cell.id.split(",")
-                temp.push(new Node(coords[0], coords[1]))
+            coords = cell.id.split(",")
+            let vertex
+            if(cell.className == "wall") {
+                vertex = new Vertex(coords[1], coords[0], false)
             }
             else {
-                temp.push(new Node(coords[0], coords[1], undefined))
+                vertex = new Vertex(coords[1], coords[0], true, cell.className=="startnode", cell.className=='endnode')
+                if (cell.className=="startnode") {
+                    //console.log("start")
+                    start = vertex
+                }
+                if(cell.className=="endnode") {
+                    //console.log("end")
+                    end = vertex
+                }
+            }
+            graph.addVertex(vertex)
+            //console.log(`Vertex: ${vertex.x},${vertex.y}`)
+            for(const key of graph.adjList.keys()) {
+                if (((key.x == vertex.x+1 || key.x == vertex.x-1) && key.y == vertex.y) || (key.x == vertex.x && (key.y == vertex.y+1 || key.y == vertex.y-1))) {
+                    //console.log(`Keymatch: ${key.x},${key.y}`)
+                    graph.addEdge(key, vertex)
+                }
             }
         })
-        nodes.push(temp)
     });
 
-    //let str = ""
-    //for(let i=0; i<nodes.length; i++)
-    //{
-    //    for(let j=0; j<nodes[i].length; j++)
-    //    {
-    //        if(nodes[i][j] !== undefined)
-    //        str = str + (`${nodes[i][j].x},${nodes[i][j].y} `)
-    //    }
-    //    str = str + "\n"
-    //}
-    //console.log(str)
     //These don't exist yet, names are placeholders
-    let start = 1
-    let end = 1
+
     //Dont forget to undo these ^^^^^
 
     const open = []
     const closed = []
 
     open.push(start)
+    start.g = 0
+    console.log(`end: ${end.x},${end.y}`)
     while (open.length != 0) {
-        open.sort((a, b) => {a.f-b.f}) //Sort ascending by cost
-        let current = open.pop()
+        console.log(`outer loop ${open.length}`)
+        open.sort((a, b) => {return a.f-b.f}) //Sort ascending by cost
+        console.log(open)
+        let current = open.shift()
+        console.log(`current: ${current.x},${current.y}`)
         closed.push(current)
+        console.log(closed)
 
-        if (current === end) //If the current node is the end node
-        {
+        if (current === end) {//If the current node is the end node
+            console.log("GOTTEM")
             let path = []
-            //TODO - Get the path from the current node to start
-            //return it
+            while(current) {
+                path.push(current)
+                current = current.parent
+            }
+            console.log(path)
+            return path
         }
 
         let children = []
         //TODO - for each child of current, append it to children
+        for (neighbor of graph.adjList.get(current)) {
+            console.log(`add child ${neighbor.x},${neighbor.y}`)
+            children.push(neighbor)
+        }
 
         //for each child
         for (child of children) { //Continue if child has already been searched
-            if(closed.some((element) => {element.coord == child.coord})) {
+            if(closed.some((element) => {return element === child})) {
+                console.log("searched")
                 continue
             }
-            child.g = current.g+1
-            child.h = (child.x * child.x) + (child.y + child.y)
-            child.f = child.g + child.h
-        }
 
-        if(open.some((element) => {element.h < child.h})) {
-            continue
+            let g = current.g+1
+            console.log(g)
+            let h = Math.abs(child.x - end.x) + Math.abs(child.y - end.y)
+            console.log(h)
+            let f = g + h
+            console.log(f)
+
+
+            if(open.some(element => {return element.x == child.x && element.y == child.y}) && child.g < g) {
+                console.log(g)
+                console.log(`already pathed ${child.x},${child.y}`)
+                continue
+            }
+            child.g = g
+            child.h = h
+            child.f = f
+            child.parent = current
+            
+            console.log(`push child ${child.x},${child.y}`)
+            open.push(child)
         }
-        open.push(child)
     }
+    return []
 }
