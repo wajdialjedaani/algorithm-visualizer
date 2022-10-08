a141736bcf367d086fe7ed6bcb53e6341b63ffff
Bryan Tang <bryanktang@gmail.com>
2022-10-08 11:12:00 -0500

    Implement zoom in/out

--- js/main.js
+++ js/main.js
@@ -10,25 +10,46 @@ function darkMode() {   // enables dark mode
     }
 }
 
-function increaseFontSize(increaseFactor) {
+function increaseFontSize() {
     var text = document.querySelectorAll('*');
     
     for(var i = 0; i < text.length; i++) {
         var style = window.getComputedStyle(text[i]).getPropertyValue('font-size');
         var currentSize = parseFloat(style);
-        text[i].style.fontSize = (currentSize + increaseFactor) + 'px';
+        text[i].style.fontSize = (currentSize + 1) + 'px';  // current size + 1 px
     }
 }
 
-function decreaseFontSize(decreaseFactor) {
+function decreaseFontSize() {
     var text = document.querySelectorAll('*');
     
     for(var i = 0; i < text.length; i++) {
         var style = window.getComputedStyle(text[i]).getPropertyValue('font-size');
         var currentSize = parseFloat(style);
-        text[i].style.fontSize = (currentSize - decreaseFactor) + 'px';
+        text[i].style.fontSize = (currentSize - 1) + 'px';  // current size + 1 px
     }
-} 
+}
+
+var zoom = 1;   // original size
+var zoomChange = 0.01;   // zoom 20%
+function zoomIn() {
+    zoom += zoomChange;
+    var elements = document.querySelectorAll('*');
+
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.transform = "scale(" + zoom + ")";
+    }
+}
+
+function zoomOut() {
+    zoom -= zoomChange;
+    var elements = document.querySelectorAll('*');
+
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.transform = "scale(" + zoom + ")";
+    }
+}
+    
 
 function sortingSelected() {
     anime({
