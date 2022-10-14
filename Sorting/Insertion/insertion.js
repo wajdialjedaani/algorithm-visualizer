17b26cccdab5d766244cc1bc731f82802c00756c
Tang, Bryan <bryantang@my.unt.edu>
2022-10-13 21:29:14 -0500

    Add step highlighting

--- Sorting/Insertion/insertion.js
+++ Sorting/Insertion/insertion.js
@@ -15,35 +15,82 @@ function generate(arr) {
 
 function insertionSort(arr) {
     let n = arr.length;
+    let steps = [];
     
     for(let i = 1; i < n; i++) {
         let current = arr[i];
         let j = i - 1;
 
-        let selected1 = '#arrBar' + i;
-        let selected2 = '#arrBar' + j;
-        // let selected = document.getElementById(selected1);
-        // console.log(selected);
-        document.querySelector(selected1).classList.toggle('arrBarSelected');
-        document.querySelector(selected1).classList.toggle('arrBarSelected');
-
-        anime({
-            targets: [selected1, selected2],
-            translateX: 100,
-            duration: 1000,
-        });
-
         while(j >= 0 && arr[j] > current) { // checks if j is outside of array and compares j position value with current
+            steps.push([i, j]);
             arr[j + 1] = arr[j];
             j--;
         }
         arr[j + 1] = current;   // once while is false, the last j position is current
-        //document.getElementById(selected1).classList.toggle('arrBarSelected');
     }
-    console.log("done");
+    //console.log("done");
+    
+    //printArr(steps);
+    swap(steps);
+    
 }
 
-function printArr(arr) {
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+  
+
+async function swap(steps) {
+    for (let i = 0; i < steps.length; i++) {
+        let selected1 = '#arrBar' + steps[i][0];
+        let selected2 = '#arrBar' + steps[i][1];
+        console.log(selected1, selected2);
+        document.querySelector(selected1).classList.toggle('arrBarSelected');
+        document.querySelector(selected2).classList.toggle('arrBarSelected');
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        document.querySelector(selected1).classList.toggle('arrBarSelected');
+        document.querySelector(selected2).classList.toggle('arrBarSelected');
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+    
+
+    // setInterval(() => {
+    //     let selected1 = '#arrBar' + steps[i][0];
+    //     let selected2 = '#arrBar' + steps[i][1];
+    //     console.log(selected1, selected2);
+    //     i++
+    // }, 2000);
+
+    // steps.forEach((el, i) => {
+        
+
+    //     setInterval(function() {
+    //         let selected1 = '#arrBar' + steps[i][0];
+    //         let selected2 = '#arrBar' + steps[i][1];
+    //         console.log('selected');
+    //     }, 1000);
+
+    //     setTimeout(() => {
+            
+    //         // document.querySelector(selected1).classList.toggle('arrBarSelected');
+    //         // document.querySelector(selected2).classList.toggle('arrBarSelected');
+    //         console.log("selected")
+
+    //         setTimeout(() => {
+    //             // document.querySelector(selected1).classList.toggle('arrBarSelected');
+    //             // document.querySelector(selected2).classList.toggle('arrBarSelected');
+    //             console.log("removed")
+    //         }, 2000 * i); 
+
+    //     }, 2000 * i);
+
+        
+        
+    // });
+}
+
+function printArr(arr) { 
     for(let i = 0; i < arr.length; i++) {
         console.log(arr[i]);
     }
