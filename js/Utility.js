a0887b1a87d1573919a1976735ce054983aa7716
Alex Tomjack <amt1309@gmail.com>
2023-11-04 22:30:04 -0500

    Added utility functions for modifying the page URL without refresh. Changing function with the dropdown buttons now updates page URL to allow page refreshes to stay on the same algorithm.

--- js/Utility.js
+++ js/Utility.js
@@ -1,4 +1,4 @@
-function debounce(func, wait, immediate) {
+export function debounce(func, wait, immediate) {
 	var timeout
 	return function() {
 		var context = this, args = arguments
@@ -13,4 +13,22 @@ function debounce(func, wait, immediate) {
 	}
 }
 
-export { debounce }
+export function insertUrlParam(key, value) {
+	if (history.pushState) {
+		let searchParams = new URLSearchParams(window.location.search);
+		searchParams.set(key, value);
+		let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
+		window.history.replaceState({path: newurl}, '', newurl);
+	}
+}
+
+// to remove the specific key
+export function removeUrlParameter(paramKey) {
+	const url = window.location.href
+	console.log("url", url)
+	var r = new URL(url)
+	r.searchParams.delete(paramKey)
+	const newUrl = r.href
+	console.log("r.href", newUrl)
+	window.history.replaceState({ path: newUrl }, '', newUrl)
+}
